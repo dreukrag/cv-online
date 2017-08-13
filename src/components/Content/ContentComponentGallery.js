5f26025c384714f8a0fa0b8ce5ecfdd4393e903c
Filipe <filipegroh@gmail.com>
2017-08-13 18:22:44 -0300

    componentes para experiencia

--- src/components/Content/ContentComponentGallery.js
+++ src/components/Content/ContentComponentGallery.js
@@ -1,33 +1,39 @@
 import React from 'react'
 import ContentComponent from './ContentComponent'
 import logo from './Boston.jpg';
-import {GalleryItem, GalleryThumbnail} from './Gallery'
+import { GalleryItem, GalleryThumbnail } from './Gallery'
 
 export class ContentComponentGallery extends React.Component {
     render = () =>
         (
             <div>
                 <ContentComponent logo={logo} caption="Experiências">
-                    <GalleryThumbnail/>
-                    <GalleryThumbnail/>
-                    <GalleryThumbnail/>
-
-
-                    <p className="lead">Medgrupo/Ventron – Desenvolvedor Front-End – 2016 – 2017</p>
-                    <p>Atuei no desenvolvimento do aplicativo medsoft para mobile, usando Ionic, Cordova e AngularJS.
-                    </p>
-                    <p>Próximo fui escolhido para fazer parte do time responsável pela portação das apostilas físicas para nossa plataforma mobile, e com foco em inovar o design para manter o diferencial medsoft tanto no materia físico quanto digital. Diferenciais como interatividade evitando uma simples cópia digital.
-                        Também criei o sistema usado para uso de temas, permitindo reuso de componentes mas aplicando estilos diferentes.</p>
-                    <p className="lead">Cadmus - Consultor - 2014 - 2016</p>
-                    <p>Participei internamente dando manutenção e desenvolvendo o nosso ERP além de criar ferramentas de controle de contas celular e integrações com sistema de pagamentos do Itaú</p>
-                    <p className="lead">Cadmus - Estagiário - 2012 - 2014</p>
-                    <p>Como estagiário fiquei responsável por dar manutenção aos sistemas internos e logo passei a cuidar do ERP além de trabalhar como consultor a serviçe da Cadmus</p>
-                    <p className="lead">Total Express/Cadmus - Consultor - 2013</p>
-                    <p>Responsável pro criar aplicação consolidando cronogramas de entregas com capacidade de rastreamento.</p>
-                    <p>Criação de processo automatizado para gerar relatórios contendo gráficos complexas</p>
-                    <p>Criação de processo automatizado para impressão de etiquetas com código de barras</p>
-                    <p className="lead">Brand Analytics/Cadmus - Consultor - 2013</p>
-                    <p>Criei um processe totalmente automatizado para análise de dados estatísticos complexos, com geração de relatório inclusive com a criação de power-points com os gráficos pronto para serem apresentados. Reduzi o tempo gasto para executar tais atividades de 1 semestre para minutos.</p>
+                    <div className="GalleryThumb__container">
+                        <GalleryThumbnail empresa="Medgrupo" logo={logo} periodo="2017" cargo="Desenvolvedor Front-End Jr">
+                            <p>Parte do time de desenvolvimento do aplicativo medsoft para mobile.</p>
+                            <p>Parte do time de desenvolvimento das parte de apostila do aplicativo mobile.</p>
+                            <p>Criei o sistema usado para uso de temas nas apostilas.</p>
+                        </GalleryThumbnail>
+                        <GalleryThumbnail empresa="Total Express" logo={logo} periodo="2013" cargo="Consultor">
+                            <p>Criei aplicação consolidando cronogramas de entregas com capacidade de rastreamento.</p>
+                            <p>Automatizei a criação de relatórios contendo gráficos complexos.</p>
+                            <p>Criei aplicação para impressão de etiquetas com código de barras.</p>
+                        </GalleryThumbnail>
+                        <GalleryThumbnail empresa="Brand Analytics" logo={logo} periodo="2013" cargo="Consultor">
+                            <p>Criei um processe totalmente automatizado para análise de dados estatísticos complexos, com geração de relatório em power-points e excel contendo os gráficos pronto para serem apresentados. Reduzi o tempo gasto para executar tais atividades de 1 semestre para minutos.</p>
+                        </GalleryThumbnail>
+                        <GalleryThumbnail empresa="Cadmus" logo={logo} periodo="2012-2017" cargo="Consultor">
+                            <p className="lead">Cadmus - Consultor - 2014 - 2016</p>
+                            <p>Manutenção do ERP.</p>
+                            <p>Desenvolvimento do ERP.</p>
+                            <p>Manutenção de bancos de dados.</p>
+                            <p>Criação de soluções para uso interno.</p>
+                            <p>Integração do ERP com o sistema de pagamentos do Itaú.</p>
+                            <p>Alocação em clientes.</p>
+                            <p className="lead">Cadmus - Estagiário - 2012 - 2014</p>
+                            <p>Como estagiário fiquei responsável por dar manutenção aos sistemas internos e logo passei a cuidar do ERP além de trabalhar como consultor a serviçe da Cadmus.</p>
+                        </GalleryThumbnail>
+                    </div>
                 </ContentComponent>
             </div>
         )
--- src/components/Content/Gallery/GalleryThumbnail.js
+++ src/components/Content/Gallery/GalleryThumbnail.js
@@ -1,16 +1,35 @@
 import React from "react"
-
+import { GalleryItem } from '.'
 export class GalleryThumbnail extends React.Component {
-
-    openModal = () => (
-        "test"
-    )
+    constructor(props) {
+        super(props)
+        this.state = { toggleModal: false }
+    }
+    openModal = () => {
+        this.setState({toggleModal:true})
+        console.log("teste");
+    }
+    closeModal = () =>{
+        this.setState({toggleModal:false})
+        
+    }
 
     render = () => (
-        <div onClick={this.openModal}>
-            <h3>Empresa</h3>
-            <h5>Período</h5>
-            <img src="" alt="" />
+        <div className="GalleryThumb__main">
+            <style dangerouslySetInnerHTML={{
+                __html: [
+                    '.GalleryThumb__backgroundAssistant:before {background-image:url("' + this.props.logo + '")}'
+                ]
+            }}>
+            </style>
+            <div className="GalleryThumb__content GalleryThumb__backgroundAssistant" onClick={this.openModal}>
+                <h3>{this.props.empresa}</h3>
+                <h5>{this.props.periodo}</h5>
+                <h5>{this.props.cargo}</h5>
+            </div>
+            <GalleryItem isModalOpen={this.state.toggleModal} modalClose={this.closeModal} titulo={this.props.empresa + " - " + this.props.periodo}>
+                {this.props.children}
+            </GalleryItem>
         </div>
     )
 
