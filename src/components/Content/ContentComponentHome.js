4c7c5926dddc9bce9c0140697f782ddae1437d59
Filipe <filipegroh@gmail.com>
2017-09-15 05:17:49 -0300

    clean up and better home

--- src/components/Content/ContentComponentHome.js
+++ src/components/Content/ContentComponentHome.js
@@ -23,28 +23,11 @@ export class ContentComponentHome extends React.Component {
                     <div className="content-component__caption-main">
                         <p className="lead">Atualmente disponível, meu interesse é trabalhar na área de front-end. Possuo experiência principalmente com React, mas também AngularJs.</p>
                         <p>Quanto aos básicos, tenho experiencia com HTML5, CSS 3.0, Node.js, Firebase e Git . Para javascript uso o padrão es6. Também possuo experiência em C#, VBA e SQL e sou familiar com Java, php, python. Apesar de apenas um hobby eu também possuo experiência com SketchUp e sou familiar com Blender.</p>
-                        <p>Tenho familiaridade com Kanban, Agile e Scrum.</p>
-                        <p>Tenho muita facilidade em aprender a usar novas ferramentas muito rapidamente.</p>
+                        <p>Conheço e uso com Kanban, Agile e Scrum.</p>
+                        <p>Tenho muita facilidade em aprender a usar novas ferramentas em pouco tempo.</p>
                     </div>
-                    {/* <div className="content-component__caption-main  animation-rtlf">
-                        <p className=" ">Filipe</p>
-                    </div>
-                    <div className="content-component__caption-main  animation-rtl10">
-                        <p className=" animation-rtl10">Alexandre</p>
-                    </div>
-                    <div className="content-component__caption-main  animation-rtl20">
-                        <p className=" animation-rtl20">Groh</p>
-                    </div>
-                    <div className="content-component__caption-sec animation-fi">
-                        <p className="lead">Atualmente disponível, meu interesse é trabalhar na área de front-end. Possuo experiência principalmente com React, mas também AngularJs.</p>
-                        <p>Quanto aos básicos, tenho experiencia com HTML5, CSS 3.0, Node.js, Firebase e Git . Para javascript uso o padrão es6. Também possuo experiência em C#, VBA e SQL e sou familiar com Java, php, python. Apesar de apenas um hobby eu também possuo experiência com SketchUp e sou familiar com Blender.</p>
-                        <p>Tenho familiaridade com Kanban, Agile e Scrum.</p>
-                        <p>Tenho muita facilidade em aprender a usar novas ferramentas muito rapidamente.</p>
-                    </div> */}
-
                 </div>
-                <div className="container-fluid content-component">
-
+                <div className="container-fluid content-component__home-spacer">
                 </div>
             </div>
         )
