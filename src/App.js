70ad45712d3f2ce4d14849323c374337ce0ebbd7
Filipe <filipegroh@gmail.com>
2017-09-15 05:17:26 -0300

    about deprecated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
         <HeaderComponent />
         <ContentComponentHome style={{marginTop: "50px"}}></ContentComponentHome>
         <ContentComponentGallery></ContentComponentGallery>
-        <ContentComponentAbout></ContentComponentAbout>
         <ContentComponentContact></ContentComponentContact>
       </div>
     );
