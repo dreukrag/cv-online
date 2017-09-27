48cc93b96c49b19af1bc2f01860d77608820fd57
Filipe <filipegroh@gmail.com>
2017-09-27 16:07:29 -0300

    Content tweaks, minor styling

--- src/components/Content/ContentComponentHome.js
+++ src/components/Content/ContentComponentHome.js
@@ -15,14 +15,14 @@ export class ContentComponentHome extends React.Component {
                 }}>
                 </style>
                 <div className="parallax parallax__blur-home large-window" >
-                    <div className="content-component__caption-brand" style={{ paddingTop:"60px",paddingLeft:"25px"}}>
+                    <div className="content-component__caption-brand" style={{ paddingTop:"70px",paddingLeft:"25px"}}>
                         <p className=" animation-rtlf">Filipe</p>
                         <p className=" animation-rtl10">Alexandre</p>
                         <p className="  animation-rtl20">Groh</p>
                     </div>
                     <div className="content-component__caption-main">
                         <p className="lead">Atualmente disponível, meu interesse é trabalhar na área de front-end. Possuo experiência principalmente com React, mas também AngularJs.</p>
-                        <p>Quanto aos básicos, tenho experiencia com HTML5, CSS 3.0, Node.js, Firebase e Git . Para javascript uso o padrão es6. Também possuo experiência em C#, VBA e SQL e sou familiar com Java, php, python. Apesar de apenas um hobby eu também possuo experiência com SketchUp e sou familiar com Blender.</p>
+                        <p>Quanto aos básicos, domino HTML5, CSS 3.0, Node.js, Firebase e Git . Para javascript uso o padrão es6. Também possuo experiência em C#, VBA e SQL e sou familiar com Java, php, python. Apesar de apenas um hobby eu também domino SketchUp e sou familiar com Blender.</p>
                         <p>Conheço e uso com Kanban, Agile e Scrum.</p>
                         <p>Tenho muita facilidade em aprender a usar novas ferramentas em pouco tempo.</p>
                     </div>
