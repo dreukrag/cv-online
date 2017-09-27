0c463fadfe452f90a5857764f1aa6c0efeed5e3c
Filipe <filipegroh@gmail.com>
2017-09-27 16:06:15 -0300

    clearing content

--- src/components/Content/ContentComponentAbout.js
+++ src/components/Content/ContentComponentAbout.js
@@ -6,9 +6,7 @@ export class ContentComponentAbout extends React.Component {
     render = () => (
         <div>
             <ContentComponent logo={logo} caption="Sobre">
-                <p>Apesar de trabalhar com TI tenho as minhas outras paixões são história, relações internacionais, defesa e meu dog.</p>
-                <p>Amo correr, gamer e modelar, sketchup sendo um grande hobby meu.</p>
-                <p>Apesar de não gostar de ficção tenho uma pequena biblioteca pessoal sobre história em constante expansão.</p>
+
             </ContentComponent>
         </div>
     )
